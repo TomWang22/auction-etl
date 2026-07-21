530c93b5394047ea573e7eab7b4ca0ce23657b8e
MelonGodTier <tomwang22@yahoo.com>
2026-07-21 12:53:13 -0400

    feat(raw): allow multiple snapshots per URL

---
+++ alembic/versions/0428279b6f87_allow_page_history.py
@@ -0,0 +1,28 @@
+"""allow page history"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+revision: str = "REPLACE_ME"
+down_revision: Union[str, Sequence[str], None] = "c4aba410158b"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.drop_constraint(
+        "page_url_key",
+        "page",
+        schema="raw",
+        type_="unique",
+    )
+
+
+def downgrade() -> None:
+    op.create_unique_constraint(
+        "page_url_key",
+        "page",
+        ["url"],
+        schema="raw",
+    )
